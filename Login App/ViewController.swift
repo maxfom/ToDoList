c9c0da152fb4c07ffa16f69a58c02431ccb1bb3f
Maxim Fomichev <max.fomiche@gmail.com>
2021-06-08 22:57:01 +0300

    Добавил элементы в ViewController, расписал функции

--- Login App/ViewController.swift
+++ Login App/ViewController.swift
@@ -10,15 +10,37 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var loginButton: UIButton!
-
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var passField: UITextField!
+    
+    var name: String = ""
+    var correct: String = "Password is true"
+    var password: String = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
-    @IBAction func loginAction() {
-        
+    @IBAction func nameField(_ sender: UITextField) {
+        name = sender.text ?? ""
     }
-
-}
-
+    
+    
+    @IBAction func passField(_ sender: UITextField) {
+        password = sender.text ?? ""
+    }
+    
+    
+    @IBAction func loginAction(_ sender: Any) {
+        if (password == "124") {
+        print(name + " - Password TRUE :)")
+        }
+        else {
+            print( name + " - Password false :(")
+        }
+    }
+    }
